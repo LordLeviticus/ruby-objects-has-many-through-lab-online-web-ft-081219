8652db5164aa78e9e5cebd747b71c50efd6d929a
Levi Borland <borland.levi@gmail.com>
2019-09-16 23:58:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,21 @@
 class Artist
   
-  attr_accessor :name 
+  attr_accessor :name, :songs 
   
   @@all = []
   
   def initialize(name)
     @name = name
     @@all << self
+    @songs = []
   end
   
   def self.all
     @@all
+  end
+  
+  def songs
+    @songs
   end
   
   
